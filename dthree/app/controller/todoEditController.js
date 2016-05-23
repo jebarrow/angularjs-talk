24fb0cfff6b23e2546f2ed9cbb62af6ccf80dc10
John Barrow <John.Barrow@sds-consulting.com>
2016-05-23 15:49:47 -0400

    added dThree to the progression

---
+++ dthree/app/controller/todoEditController.js
@@ -0,0 +1,67 @@
+﻿(function () {
+    'use strict';
+    var newId = '00000000-0000-0000-0000-000000000000';
+    angular
+        .module('app')
+        .controller('todoEditController', todoEditController);
+
+    todoEditController.$inject = ['$http', '$scope'];
+    function todoEditController($http, $scope) {
+        var vm = this;
+        vm.todo = { id: newId };
+        vm.master = vm.todo;
+        vm.getResource = getResource;
+        vm.update = update;
+        vm.reset = reset;
+        vm.addTodo = addTodo;
+
+        function getResource(id) {
+            $http.get.get("/api/todo")
+            .then( function(response) {
+                vm.todo = response.data;
+                vm.todo.startDate = new Date(todo.startDate);
+                vm.todo.stopDate = new Date(todo.stopDate);
+                vm.master = todo;
+            });
+        };
+
+        function update(form) {
+            if (!form.$valid) return;
+            if (vm.todo.id === newId) {
+                $http.post("/api/todo/" + vm.todo.id, vm.todo)
+                .then(function(response){
+                  var todo = response.data;
+                  vm.getResource(todo.id);
+                });
+            } else {
+                $http.put("/api/todo/" + vm.todo.id, vm.todo)
+                .then(function(response){
+                  var todo = response.data;
+                  vm.getResource(todo.id);
+                  if (form) {
+                      form.$setPristine();
+                      form.$setUntouched();
+                  }
+                });
+            }
+        };
+
+        function reset(form) {
+            if (form) {
+                form.$setPristine();
+                form.$setUntouched();
+            }
+            vm.todo = angular.copy(vm.master);
+        };
+
+        function addTodo(form) {
+            vm.todo = {
+                id: newId,
+            };
+            if (form) {
+                form.$setPristine();
+                form.$setUntouched();
+            }
+        }
+    };
+})();
---
+++ dthree/app/controller/todoListController.js
@@ -0,0 +1,30 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('todoListController', todoListController);
+
+    todoListController.$inject = ['$http', '$scope'];
+    function todoListController($http, $scope) {
+        var vm = this;
+        vm.todoList = [];
+        vm.delete = del;
+        updateList();
+        function updateList(f) {
+          $http.get("/api/todo")
+          .then(function(response) {
+            vm.todoList = response.data;
+          });
+        }
+
+        function del(id){
+          $http.delete("/api/todo/" + id)
+          .then(function(response){
+            alert(response.statusText);
+            updateList();
+          })
+        }
+    };
+
+})();
